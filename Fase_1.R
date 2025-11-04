6556431ea31bf7507f80d586dd8866263c131dcd
Rodrigo Eduardo Hernández Morales <h.rodrigo81@yahoo.com>
2025-11-03 18:32:06 -0600

    limpieza de datos

--- Fase_1.R
+++ Fase_1.R
@@ -1,8 +1,10 @@
 library(readxl)
+library(dplyr)
+library(stringi)
 
-ruta <- "C:/Users/rodri/OneDrive/Documentos/Maestria/Cuarto_trimestre/Mineria de datos/Proyecto/datasets"
+ruta<- "C:/Users/rodri/OneDrive/Documentos/Maestria/Cuarto_trimestre/Mineria de datos/Proyecto/datasets"
 
-archivos <- list.files(path = ruta, pattern = "\\.xlsx$", full.names = TRUE)
+archivos<- list.files(path = ruta, pattern = "\\.xlsx$", full.names = TRUE)
 
 for (archivo in archivos) {
   base<- basename(archivo)
@@ -15,7 +17,6 @@ for (archivo in archivos) {
 
 ls()
 
-
 df_2018<- as.data.frame(df_2018)
 df_2019<- as.data.frame(df_2019)
 df_2020<- as.data.frame(df_2020)
@@ -23,5 +24,38 @@ df_2021<- as.data.frame(df_2021)
 df_2022<- as.data.frame(df_2022)
 df_2023<- as.data.frame(df_2023)
 df_2024<- as.data.frame(df_2024)
+
+df_final<- bind_rows(df_2018, df_2019, df_2020, df_2021, df_2022, df_2023, df_2024)
+
+### voy a usar informacion desde 2020 para quitar el efecto pandemia ademas que la informacion no es la misma
+length(colnames(df_2024))
+
+anios <- 2020:2024
+
+### voy a eliminar las filas que solo aparecen en algunos df ya que no me sirven de mucho
+### voy a unificar los nombres de las variables ya que algunas varian por algunas mayusculas o tildes
+for (i in anios) {
+  df <- get(paste0("df_", i))
+  
+  names(df) <- tolower(stri_trans_general(names(df), "Latin-ASCII"))
+  
+  df <- df %>%
+    rename_with(~ gsub("gran_grupos|gran_grupos", "gran_grupos", .x)) %>%
+    rename_with(~ gsub("subg_primarios|subg_principales", "subg_principales", .x)) %>%
+    rename_with(~ gsub("g_primarios", "g_primarios", .x))
+  
+  df <- df %>% select(-any_of(c("edad_quinquenales", "ocupacionhabitual")))
+  
+  cols_a_texto <- c("area_geo_inf")
+  for (col in cols_a_texto) {
+    if (col %in% names(df)) {
+      df[[col]] <- as.character(df[[col]])
+    }
+  }
+  
+  assign(paste0("df_", i), df)
+}
+
+df_final <- bind_rows(mget(paste0("df_", anios)))
 
 
